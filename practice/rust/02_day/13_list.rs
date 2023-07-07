3f04432d5e3c7a1d0ecc76879710f33590017276
lingllqs <lingllqs@163.com>
2023-07-07 22:55:30 +0800

    update

---
+++ practice/rust/02_day/13_list.rs
@@ -0,0 +1,48 @@
+use List::*;
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
+
+fn main() {
+    // 创建一个空链表
+    let mut list = List::new();
+
+    // 追加一些元素
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    // 显示链表的最后状态
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}
