9e1ec2995c73bb9a1d36182e8ef1c3dfea7161a9
lingllqs <lingllqs@163.com>
2023-09-09 07:24:39 +0800

    rust practice

--- practice/rust/02_day/29_bind.rs
+++ practice/rust/02_day/29_bind.rs
@@ -1,7 +1,33 @@
+#![allow(dead_code)]
+
 fn age() -> u32 {
-    15
+    10
+}
+
+fn test_01() {
+    println!("Tell me what type of person you are");
+
+    match age() {
+        0 => println!("I haven't celebrated my first birthday yet"),
+        n @ 1..=12 => println!("I'm child of age {:?}", n),
+        n @ 13..=18 => println!("I'm a teen of age {:?}", n),
+        n => println!("I'm an old person of age {:?}", n),
+    }
+}
+
+fn some_number() -> Option<u32> {
+    Some(55)
+}
+
+fn test_02() {
+    match some_number() {
+        Some(n @ 44) => println!("The Answer: {}", n),
+        // Some(55) => println!("55"),
+        Some(n) => println!("Not interesting... {}", n),
+        _ => (),
+    }
 }
 
 fn main() {
-    println!("Tell me what type of person you are");
+    test_02();
 }
